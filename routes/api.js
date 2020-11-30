de314b7410d2a27c0d356b7a12537cb8cae4b34e
Owusu <adjeibohyen@hotmail.co.uk>
2020-11-30 19:41:35 +0000

    post route

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/", function (req, res, next) {
+  res.send(req.body);
 });
 
 module.exports = router;
